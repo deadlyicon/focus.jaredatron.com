3eb51668a4eac9a07e4e8e4beac1f290c870740a
Jared Grippe <jared@deadlyicon.com>
2014-03-10 20:33:12 -0700

    WIP

---
+++ app/assets/javascripts/routes/index_route.js
@@ -0,0 +1,5 @@
+// For more information see: http://emberjs.com/guides/routing/
+
+Focus.IndexRoute = Ember.Route.extend({
+
+});
---
+++ app/assets/javascripts/views/index_view.js
@@ -0,0 +1,6 @@
+// for more details see: http://emberjs.com/guides/views/
+
+Focus.IndexView = Ember.View.extend({
+  templateName: 'index',
+  classNames: ['page'],
+});
