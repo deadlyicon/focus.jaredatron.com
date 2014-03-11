12b57711049595ae897db1c9d22fc6ead8842e20
Jared Grippe <jared@deadlyicon.com>
2014-03-10 17:17:45 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FocusJaredatronCom::Application.config.secret_key_base = 'ce7c7af8a49fd683723249188b8197462ce15c3e81181b46560261b54a35fcf496c36cf90c2cfa2fef3b75c96ba4f92ced36bd2b50bfbce296cc80984cc41fb6'
